538dccbf352a4a1c02487d22c361902ae627c0c9
Christian Kakesa <christian.kakesa@gmail.com>
2026-06-12 01:16:30 +0800

    oid company entry generator

---
+++ cmd/oid/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+const templateStr = `
+{
+    "_id": { "$oid": "%s" },
+    "stats_show": 0,
+    "name": "",
+    "activity": "",
+    "description": "",
+    "slogan": "",
+    "address_line_1": "",
+    "address_line_2": "",
+    "city": "",
+    "postal_code": "",
+    "po_box": "",
+    "country": "",
+    "main_phone": "",
+    "alt_phone": "",
+    "mobile_phone": "",
+    "email": "",
+    "facebook": "",
+    "linkedin": "",
+    "instagram": "",
+	"tiktok": "",
+	"whatsapp":"",
+    "youtube": "",
+    "website": "",
+    "published": true,
+    "name_seo": "",
+    "geo": null,
+    "updated_at": null,
+    "created_at": { "$date": "%s" }
+  }
+`
+
+func main() {
+	oid := primitive.NewObjectID().Hex()
+	crrated_at := time.Now().Format("2006-01-02T15:04:05.000Z")
+	fmt.Fprintf(os.Stdout, templateStr, oid, crrated_at)
+}
